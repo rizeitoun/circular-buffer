8f6fab52a1a929246cc7240ae9cbb1103d4a2712
rizeitoun <48197623+rizeitoun@users.noreply.github.com>
2019-03-04 19:20:55 -0800

    Create CircularBuffer.cs

---
+++ CircularBuffer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Creates a first-in first-out generic buffer with a defined maximum size.  If buffer is full, error for writing a new element.  Reading buffer removes first item from it.
+public class CircularBuffer<T>
+{
+    private Queue<T> queue = new Queue<T>();
+    private int capacity;
+
+    public CircularBuffer(int capacity)
+    {
+        this.capacity = capacity;
+    }
+
+    public T Read()
+    {
+        if (queue.Count > 0)
+        {
+            return queue.Dequeue();
+        }
+        throw new InvalidOperationException();
+    }
+
+    public void Write(T value)
+    {
+        if (queue.Count < capacity)
+        {
+            queue.Enqueue(value);
+        }
+        else
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    public void Overwrite(T value)
+    {
+        if (queue.Count == capacity)
+        {
+            queue.Dequeue();
+        }
+        queue.Enqueue(value);
+    }
+
+    public void Clear()
+    {
+        queue.Clear();
+    }
+}
